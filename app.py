1e5e9168318d7f7861713b94c77c0b9a15932ac7
Ksenia Lukin <90192541+kslukin@users.noreply.github.com>
2025-06-05 10:53:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown(
     <style>
     /* Background image */
     .stApp {
-        background-image: url("https://images.unsplash.com/vector-1739847838117-ed04bd52ed18?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-image: url("https://images.unsplash.com/vector-1747674822525-7c0cfff8a9af?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
